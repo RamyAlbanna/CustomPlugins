96f98b51b45ac711c6a32e34c99e9920c06d6995
ramy elbanna <ramyalbanna12@gmail.com>
2025-01-17 21:33:51 +0200

    adding salary validator and contact external data classes

---
+++ ContactExternalData.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+public class ContactExternalData : IPlugin
+{
+    public void Execute(IServiceProvider serviceProvider)
+    {
+        IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+        IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+        IOrganizationService service = serviceFactory.CreateOrganizationService(context.InitiatingUserId);
+
+        using (HttpClient client = new HttpClient())
+        {
+            client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.GetAsync("users").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var result = response.Content.ReadAsStringAsync().Result;
+                context.OutputParameters["StringOutput"] = result;
+            }
+            else
+            {
+                Console.WriteLine("Internal server Error");
+            }
+        }
+    }
+}
---
+++ SalaryValidator.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+public class SalaryValidator : IPlugin
+{
+    public void Execute(IServiceProvider serviceProvider)
+    {
+        IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+        if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity targetEntity)
+        {
+            if (targetEntity.LogicalName == "contact" && targetEntity.Attributes.Contains("ram_salary"))
+            {
+                Money newSalaryMoney = (Money)targetEntity["ram_salary"];
+                decimal newSalaryValue = newSalaryMoney.Value;
+                Money oldSalaryMoney = (Money)context.PreEntityImages["ram"]["ram_salary"];
+                decimal oldSalaryValue = oldSalaryMoney.Value;
+                if (Math.Abs(newSalaryValue - oldSalaryValue) > 3000)
+                {
+                    throw new InvalidPluginExecutionException("Salary change must not exceed ±3000.");
+                }
+            }
+        }
+    }
+}
